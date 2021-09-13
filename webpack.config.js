057bd0741ebdba23c233bc4d1886fc60125571cf
Luphia Chang <luphia@cafeca.io>
2021-09-13 12:03:20 +0800

    Update Structure

--- webpack.config.js
+++ webpack.config.js
@@ -3,69 +3,16 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const frontend = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src/frontend/main.js'),
-  output: {
-    path: path.resolve(__dirname, 'build/frontend'),
-    filename: 'js/main.js',
-    chunkFilename: '[id].js'
-  },
-  devServer: {
-    watchOptions: {
-      poll: true,
-    },
-    contentBase: path.join(__dirname, "build/frontend"),
-    compress: true,
-    port: 9000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: [
-          "babel-loader"
-        ]
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "assets/[name].[ext]", // 修改生成路徑
-              esModule: false,
-              publicPath: "../",
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      Buffer: ["buffer", "Buffer"],
-    }),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "css/[name].css",
-      chunkFilename: "css/[id].css",
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-  devtool: "cheap-module-source-map",
+const static = {
+
 };
 
-module.exports = [
-  frontend
-];
+const frontend = {
+
+};
+
+const background = {
+
+};
+
+module.exports = [static, frontend, background];
